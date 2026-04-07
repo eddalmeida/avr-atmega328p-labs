20125ca38ba884f0762845751822ebb6b5b371ec
Edson Almeida <edson4323macedo2@gmail.com>
2026-04-07 01:09:33 -0300

    Reorganiza árvore do projeto e adiciona Makefile próprio

--- blink-atmega328p/src/main.c
+++ blink-atmega328p/src/main.c
@@ -5,21 +5,17 @@
  *  Author: edson macedo de almeida 
  */ 
 
-#define F_CPU 16000000UL   // Define clock em 16 MHz
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
 {
 	DDRB = 0xFF;          // Configura todos os pinos do PORTB como saída
-	PORTB = 0x00;         // Inicializa todos os pinos em LOW
+	PORTB = 0x00;         // Inicializa todos os pinos do PORTB LOW
 
 	while (1)
 	{
-		PORTB |= (1 << 5);   // Liga LED no pino PB5 (Arduino UNO = LED_BUILTIN)
-		_delay_ms(1000);
-
-		PORTB &= ~(1 << 5);  // Desliga LED no PB5
+		PORTB ^= (1 << 5);   // troca estado pino PB5 (Arduino UNO = LED_BUILTIN)
 		_delay_ms(1000);
 	}
 	return 0;
--- muxDisplay7seg/src/main.c
+++ muxDisplay7seg/src/main.c
@@ -1,25 +1,26 @@
-#define F_CPU 16000000UL                                           //define frequencia 16Mhz
 #include <avr/io.h>
 #include "avr/pgmspace.h"                                          //Biblioteca para escrever na flash
 #include <util/delay.h>                                            //Biblioteca para utilizar delay
+#include <stdint.h>
 
-#define read(PINx, Px) ((PINx & (1 << Px)) >> Px)                 //macro para ler botão e retornar 1 ou 0
+
+#define read(PINx, Px) ((PINx & (1 << Px)) >> Px)                 //macro para ler botï¿½o e retornar 1 ou 0
 #define set_bit(PORTx, Px) (PORTx |= (1 << Px))                    //macro para colocar pino como alto
 #define clr_bit(PORTx, Px) (PORTx &= !(1 << Px))                   //macro para colocar pino como baixo
 
-#define btn1 PB2                                                   //define botão1 no PB0
-#define btn2 PB3                                                   //define botão2 no PB0
+#define btn1 PB2                                                   //define botï¿½o1 no PB0
+#define btn2 PB3                                                   //define botï¿½o2 no PB0
 #define DISPLAY PORTD                                              //define display no PORTD
 
 
 
 const unsigned int numero[] PROGMEM = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71}; // Tabela para display
 
-unsigned int estadoant1 = 1;                                         //amarzena estado anterior do botão1 para ler apenas 1 vez
-unsigned int estadoant2 = 1;                                         //amarzena estado anterior do botão2 para ler apenas 1 vez
+uint8_t estadoant1 = 1;                                              //amarzena estado anterior do botï¿½o1 para ler apenas 1 vez
+uint8_t estadoant2 = 1;                                              //amarzena estado anterior do botï¿½o2 para ler apenas 1 vez
 
 
-void display_print(int valor);                                       //declaração da função para pintar no display
+void display_print(int valor);                                       //declaraï¿½ï¿½o da funï¿½ï¿½o para pintar no display
 
 
 int main(void)
@@ -30,16 +31,16 @@ int main(void)
 	PORTD = 0x00;                                                    //Inicia PORTD como 0
 	UCSR0B = 0x00;                                                   //Condigura PD0 e PD1 como I/O
 	
-	unsigned int contador = 0;                                       //variavel contador
+	uint8_t contador = 0;                                       //variavel contador
 	
     while (1) 
     {
 		
-		unsigned botao1 = read(PINB, btn1);                          //armazena valor do botao1 
-		unsigned botao2 = read(PINB, btn2);                          //amarzena estado anterior do botão para ler apenas 1 vez
+		uint8_t botao1 = read(PINB, btn1);                          //armazena valor do botao1 
+		uint8_t botao2 = read(PINB, btn2);                          //amarzena estado anterior do botï¿½o para ler apenas 1 vez
 		
 		
-		if (botao1 == 0 && estadoant1 == 1)                           //leitura botão de decrementar
+		if (botao1 == 0 && estadoant1 == 1)                           //leitura botï¿½o de decrementar
 		{
 			contador++;
 			_delay_ms(10);
@@ -47,7 +48,7 @@ int main(void)
 		}
 		
 		   
-		if (botao2 == 0 && estadoant2 == 1)                           //leitura de botão de decrementar 
+		if (botao2 == 0 && estadoant2 == 1)                           //leitura de botï¿½o de decrementar 
 		{ 
 			contador--;
 			_delay_ms(10);
@@ -57,15 +58,15 @@ int main(void)
 		if(contador > 99) contador = 0;                               //contador = 0 se for negativo ou > 99
 		
 		
-		estadoant2 = botao2;                                          //amarzena estado anterior do botão1
-		estadoant1 = botao1;                                          //amarzena estado anterior do botão1
+		estadoant2 = botao2;                                          //amarzena estado anterior do botï¿½o1
+		estadoant1 = botao1;                                          //amarzena estado anterior do botï¿½o1
 		
 		
 		display_print(contador);
     }
 }
 
-void display_print(int valor)                                         //função multiplexação display 7 segmentos
+void display_print(int valor)                                         //funï¿½ï¿½o multiplexaï¿½ï¿½o display 7 segmentos
 {
 	int digito1 = valor / 10;                                         // separa o numero mais significativo
 	int digito2 = valor - (digito1 * 10);                             // separa o numero menos significativo
@@ -75,13 +76,13 @@ void display_print(int valor)                                         //função m
 	set_bit(PORTB, PB0);                                              // seleciona para escrever no display da direita
 	DISPLAY =  pgm_read_byte(&numero[digito2]);
 	
-	_delay_ms(50);
+	_delay_ms(5);
 	
 	DISPLAY = 0x00;
 	clr_bit(PORTB, PB0);
 	set_bit(PORTB, PB1);                                              // seleciona para escrever no display da esquerda 
 	DISPLAY =  pgm_read_byte(&numero[digito1]);
 	
-	_delay_ms(50);
+	_delay_ms(5);
 			
 }
